462d26c0acc99fdf419876c8cb3ebfc9da21a4b9
mijail <mijail86@gmail.com>
2023-11-25 19:31:55 -0500

    validancion de tipos de datos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Body, Query
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
@@ -11,11 +11,25 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(ge=1, le=2023)
+    rating: float = Field(ge= 0.1, le=10.0)
+    category: str = Field(min_length=3, max_length=15)
+
+    #Esto es para los campos por default
+    class Config:
+        #esto cambio en la V2 de fastapi, antes se colocaba solo schema_extra
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Mi pelicula",
+                "overview": "Descripcion de la pelicula",
+                "year": 2023,
+                "rating": 9.8,
+                "category": "Acción"
+            }
+        }
 
 movies = [
     {
