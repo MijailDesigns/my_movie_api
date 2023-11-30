6390ae1fba5e085a3c6969e8cb8bd06f1ec217bb
mijail <mijail86@gmail.com>
2023-11-29 19:29:59 -0500

    authentication with pyjwt

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI, HTTPException, Body, Query, Path
-from fastapi.responses import HTMLResponse
+from fastapi import Depends, FastAPI, HTTPException, Body, Query, Path, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, List
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 app = FastAPI()
 #Para cambiar el nombre de la aplicacion
 app.title = "My app with FastAPI"
 #Para cambiar la version de la aplicacion
 app.version = "0.0.1"
+
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth = await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != "admin@gmail.com":
+            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
+
+class User(BaseModel):
+    email: str
+    password: str
 
 class Movie(BaseModel):
     id: Optional[int] = None
@@ -55,29 +68,39 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello World</h1>')
 
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token = create_token(user.model_dump())
+        return JSONResponse(status_code=200, content=token)
+    else:
+        return JSONResponse(status_code=404, content={"message": "Credenciales inválidas, intente de nuevo"})
 
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int = Path(ge=1, le=2000)):
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
+    return JSONResponse(status_code=200, content=movies)
+
+@app.get('/movies/{id}', tags=['movies'], response_model=Movie, status_code=200)
+def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     try:
-        return list(filter(lambda movie: movie['id'] == id, movies))[0]
+        movie = list(filter(lambda movie: movie['id'] == id, movies))[0]
+        return JSONResponse(status_code=200, content=movie)
     except IndexError as error:
         print(error)
         raise HTTPException(status_code=404, detail=f"Movie with id {id} not found")
     
-@app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category:str = Query(min_length=5, max_length=15)):
-    return [item for item in movies if item['category'] == category]
+@app.get('/movies/', tags=['movies'], response_model=List[Movie], status_code=200)
+def get_movies_by_category(category:str = Query(min_length=5, max_length=15)) -> List[Movie]:
+    data = [item for item in movies if item['category'] == category]
+    return JSONResponse(status_code=200, content=data)
 
-@app.post('/movies', tags=['movies'])
-def create_movie(movie: Movie):
+@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
     movies.append(movie)
-    return movies
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado la pelicula"})
 
-@app.put('/movies/{id}', tags=['movies'])
-def update_movie(id:int, movie: Movie = Body()):
+@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def update_movie(id:int, movie: Movie = Body()) -> dict:
     for item in movies:
         if item['id'] == id:
             item['title'] = movie.title
@@ -85,15 +108,15 @@ def update_movie(id:int, movie: Movie = Body()):
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-            return movies
+            return JSONResponse(status_code=200, content={"message": "Se ha modificado la pelicula"})
     raise HTTPException(status_code=404, detail=f"Movie with id {id} not found")
 
-@app.delete('/movies/{id}', tags=['movies'])
-def delete_movie(id:int):
+@app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def delete_movie(id:int) -> dict:
     for item in movies:
         if item['id'] == id:
             movies.remove(item)
-            return movies
+            return JSONResponse(status_code=200, content={"message": "Se ha eliminado la pelicula"})
     raise HTTPException(status_code=404, detail=f"Movie with id {id} not found")
     
 
